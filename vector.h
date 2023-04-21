505d64d05f6a602d989806bb268e59becd02d611
1327295886 <1327295886@qq.com>
2023-04-21 14:32:16 +0800

    更改

--- vector.h
+++ vector.h
@@ -5,6 +5,10 @@
 
 namespace MySTL
 {
+	/*
+	vector维护一个连续线性的空间，无论是什么元素，普通指针都可以当作vector的迭代器使用(vector迭代器的所有操作普通指针都具备)
+	vector提供的是Random Access Iterators
+	*/
 	template<class T, class Alloc = alloc>
 	class vector
 	{
@@ -22,8 +26,11 @@ namespace MySTL
 		// 此处是二级配置器
 		typedef simple_alloc<value_type, alloc> data_allocator;
 
+		// 目前使用的空间的头
 		iterator start;
+		// 目前使用的空间的尾
 		iterator finish;
+		// 目前可使用的空间的尾
 		iterator end_of_storage;
 
 		void insert_aux(iterator position, const T& x);
@@ -46,6 +53,19 @@ namespace MySTL
 			uninitialized_fill_n(result, s, x);
 			return result;
 		}
+
+	public:
+		// 首尾判断
+		iterator begin() { return start; }
+		const_iterator begin() const { return start; }
+		iterator end() { return finish; }
+		const_iterator end() const { return finish; }
+
+		// 大小,强制转化陈size_type
+		size_type size() { return size_type(end() - begin()); }
+
+
+
 	};
 }
 	
